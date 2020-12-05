93c2bfc2835a68a3325d5ad8a64ecb0865a2091a
GunterPearson <1715@holbertonschool.com>
2020-12-04 21:13:42 -0600

    hopefully fixed issue with email

--- web_flask/home.py
+++ web_flask/home.py
@@ -5,22 +5,11 @@ import os
 from flask_cors import CORS
 from web_flask.forms import ContactForm
 from flask_mail import Message, Mail
+import smtplib
 app = Flask(__name__)
 cors = CORS(app, resources={r"": {"origins": "*"}})
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-# app.config['MAIL_DEBUG'] = True
-app.config['MAIL_USERNAME'] = 'gunter10pearson@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Gunter$2000'
-app.config['MAIL_DEFAULT_SENDER'] = None
-app.config['MAIL_MAX_SEND'] = None
-app.config['MAIL_SUPPRESS_SEND'] = False
-app.config['MAIL_ASCII_ATTATCHMENTS'] = False
-mail = Mail(app)
 
 
 @app.route('/', strict_slashes=False, methods=['GET', 'POST'])
@@ -28,10 +17,25 @@ def home():
     """home route for html file"""
     form = ContactForm()
     if request.method == "POST":
-        msg = Message(form.subject.data, sender=form.email.data, recipients=['howaj26162@hebgsw.com'])
-        msg.body = form.name.data + "\n" + form.message.data + "\n\n" + form.email.data
-        mail.send(msg)
-        flash("Message is Sent!")
+        TO = "howaj26162@hebgsw.com"
+        SUBJECT = form.subject.data
+        TEXT = form.name.data + "\n" + form.message.data + "\n\n" + form.email.data
+        # Gmail Sign In
+        gmail_sender = 'gunter10pearson@gmail.com'
+        gmail_passwd = 'Gunter$2000'
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(gmail_sender, gmail_passwd)
+        BODY = '\r\n'.join(['To: %s' % TO,
+                            'From: %s' % gmail_sender,
+                            'Subject: %s' % SUBJECT,
+                            '', TEXT])
+        try:
+            server.sendmail(gmail_sender, [TO], BODY)
+        except:
+            pass
+        server.quit()
     return render_template("home.html", form=form)
 
 @app.route('/contact', strict_slashes=False, methods=['GET', 'POST'])
