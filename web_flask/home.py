4b7e3c4a515f847bcde25fa363a3d7b12766a664
GunterPearson <1715@holbertonschool.com>
2020-12-07 20:23:39 -0600

    pushing file home.py

--- web_flask/home.py
+++ web_flask/home.py
@@ -10,6 +10,19 @@ app = Flask(__name__)
 cors = CORS(app, resources={r"": {"origins": "*"}})
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_DEBUG'] = True
+app.config['MAIL_USERNAME'] = 'gunter10pearson@gmail.com'
+app.config['MAIL_PASSWORD'] = 'StoneCo$$'
+app.config['MAIL_DEFAULT_SENDER'] = None
+app.config['MAIL_MAX_SEND'] = None
+app.config['MAIL_SUPPRESS_SEND'] = False
+app.config['MAIL_ASCII_ATTATCHMENTS'] = False
+mail = Mail(app)
 
 
 @app.route('/', strict_slashes=False, methods=['GET', 'POST'])
@@ -17,25 +30,10 @@ def home():
     """home route for html file"""
     form = ContactForm()
     if request.method == "POST":
-        TO = "howaj26162@hebgsw.com"
-        SUBJECT = form.subject.data
-        TEXT = form.name.data + "\n" + form.message.data + "\n\n" + form.email.data
-        # Gmail Sign In
-        gmail_sender = 'gunter10pearson@gmail.com'
-        gmail_passwd = 'StoneCo$$'
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.ehlo()
-        server.starttls()
-        server.login(gmail_sender, gmail_passwd)
-        BODY = '\r\n'.join(['To: %s' % TO,
-                            'From: %s' % gmail_sender,
-                            'Subject: %s' % SUBJECT,
-                            '', TEXT])
-        try:
-            server.sendmail(gmail_sender, [TO], BODY)
-        except:
-            pass
-        server.quit()
+        msg = Message(form.subject.data, sender=form.email.data, recipients=['howaj26162@hebgsw.com'])
+        msg.body = form.name.data + "\n" + form.message.data + "\n\n" + form.email.data
+        mail.send(msg)
+        flash("Message is Sent!")
     return render_template("home.html", form=form)
 
 @app.route('/contact', strict_slashes=False, methods=['GET', 'POST'])
